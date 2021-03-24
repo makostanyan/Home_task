944d15a7cf7691c8a0c073daea68e1d4bf68aa09
Mane Kostanyan <Mane_kostanyan@epam.com>
2021-03-24 17:29:00 +0400

    Fixed all comments

--- src/test/java/GreetzTest.java
+++ src/test/java/GreetzTest.java
@@ -4,14 +4,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import java.util.List;
+import java.util.Random;
 
-public class greetzTest {
+public class GreetzTest {
 
-    private WebDriver driver;
-    private String email = "HiHello@gmail.com";
-    private String password = "HiHello888";
+    WebDriver driver;
 
     private void loginToPage() throws InterruptedException {
+
+        String email = "HiHello@gmail.com";
+        String password = "HiHello888";
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -30,19 +34,21 @@ public class greetzTest {
         loginToPage();
         driver.get("https://www.greetz.nl/ballonnen/denken-aan");
         Thread.sleep(30000);
-        String expectedPrice = driver.findElement(By.xpath("//a[@data-id='1142795629']//span[@data-qa-ref='normal-price']")).getText();
-        String expectedName = driver.findElement(By.xpath("//a[@data-id='1142795629']//div[@class='b-products-grid__item-title']")).
-                getText().toLowerCase();
-        driver.findElement(By.xpath("//a[@data-id='1142795629']/..//a/div[@class='b-favourite']")).click();
+        WebElement productComponents = driver.findElement(By.cssSelector("div.b-products-grid__item:nth-child(1)"));
+        String expectedPrice = productComponents.findElement(By.className("b-products-grid__item-price")).getText() ;
+        String expectedName = productComponents.findElement(By.className("b-products-grid__item-title")).getText().toLowerCase();
+        productComponents.findElement(By.className("b-favourite")).click();
         driver.findElement(By.xpath("//i[@data-qa-ref= 'profile-icon']")).click();
         driver.findElement(By.xpath("//span[text() = 'Favorieten']")).click();
-        Thread.sleep(2000);
+        Thread.sleep(3000);
         driver.findElement(By.xpath("//div[@class= 'center full-size']")).click();
         Thread.sleep(2000);
         String actualName = driver.findElement(By.xpath("//form[@name= 'productAmountForm']/h1")).getText().toLowerCase();
-        Assert.assertEquals(actualName, expectedName, "Names are the same");
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(actualName, expectedName, "Names are different");
         String actualPrice = driver.findElement(By.className("price-normal")).getText().substring(2);
-        Assert.assertEquals(actualPrice, expectedPrice, "Prices are equals");
+        softAssert.assertEquals(actualPrice, expectedPrice, "Prices are different");
+        softAssert.assertAll();
         driver.findElement(By.xpath("//div[text() =  'Toegevoegd aan favorieten']")).click();
         driver.quit();
     }
@@ -53,15 +59,19 @@ public class greetzTest {
         loginToPage();
         driver.get("https://www.greetz.nl/kaarten/denken-aan");
         Thread.sleep(5000);
-        driver.findElement(By.cssSelector("[data-id='3000011108']")).click();
-        Thread.sleep(2000);
+        List<WebElement> results = driver.findElements(By.xpath("//a[@data-qa-ref='product-wall-item']"));
+        Thread.sleep(3000);
+        Random random = new Random();
+        results.get(random.nextInt(results.size())).click();
+        Thread.sleep(3000);
         WebElement element = driver.findElement(By.xpath("//div[@class='page-detail__select']//div//input"));
         element.clear();
         element.sendKeys("2");
         String perPrice = driver.findElement(By.xpath("//g-price[@class='price-card price-block']//span[@class='price-normal']")).getText();
-        String doublePerPrice = String.valueOf(Double.parseDouble((perPrice.substring(2).replace(",", "."))) * 2);
-        String totalPrice = driver.findElement(By.xpath("//span//div[@class='price-total']")).getText().replace(',', '.');
-        Assert.assertTrue(totalPrice.contains(doublePerPrice), "Total price is correct");
+        double doublePerPrice = Double.parseDouble(perPrice.replaceAll("[^\\d,]", "").replace(',', '.'))* 2;
+        String totalPrice = driver.findElement(By.xpath("//span//div[@class='price-total']")).getText();
+        double doubleTotalPrice = Double.parseDouble(totalPrice.replaceAll("[^\\d,]", "").replace(',', '.'));
+        Assert.assertEquals(doublePerPrice, doubleTotalPrice, "Total amount is incorrect");
         driver.quit();
     }
 }
